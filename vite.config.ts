ba5c1829842ee6f942495565134547524425cde8
lucas astesiano <lucasagustinastesiano@gmail.com>
2024-05-20 15:12:22 -0300

    cambio en vite config

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base:'https://lucasastesiano.github.com/DH-Ecomerce'
+  base:'https://lucasastesiano.github.io/DH-Ecomerce'
 })
